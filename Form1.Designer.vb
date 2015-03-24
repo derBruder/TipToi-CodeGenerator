456a5990405bb53be9c91939c0ecaaaaf157bb86
derDaniel <GIT@wockels.de>
2015-03-24 16:24:32 +0100

    Add some more files

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+    Me.Button1 = New System.Windows.Forms.Button()
+    Me.txtValue = New System.Windows.Forms.TextBox()
+    Me.Button2 = New System.Windows.Forms.Button()
+    Me.Label1 = New System.Windows.Forms.Label()
+    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'PictureBox1
+    '
+    Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+    Me.PictureBox1.Name = "PictureBox1"
+    Me.PictureBox1.Size = New System.Drawing.Size(559, 516)
+    Me.PictureBox1.TabIndex = 0
+    Me.PictureBox1.TabStop = False
+    '
+    'Button1
+    '
+    Me.Button1.Location = New System.Drawing.Point(587, 64)
+    Me.Button1.Name = "Button1"
+    Me.Button1.Size = New System.Drawing.Size(100, 23)
+    Me.Button1.TabIndex = 1
+    Me.Button1.Text = "GO!"
+    Me.Button1.UseVisualStyleBackColor = True
+    '
+    'txtValue
+    '
+    Me.txtValue.Location = New System.Drawing.Point(587, 38)
+    Me.txtValue.Name = "txtValue"
+    Me.txtValue.Size = New System.Drawing.Size(100, 20)
+    Me.txtValue.TabIndex = 2
+    '
+    'Button2
+    '
+    Me.Button2.Location = New System.Drawing.Point(700, 35)
+    Me.Button2.Name = "Button2"
+    Me.Button2.Size = New System.Drawing.Size(75, 23)
+    Me.Button2.TabIndex = 3
+    Me.Button2.Text = "+"
+    Me.Button2.UseVisualStyleBackColor = True
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label1.Location = New System.Drawing.Point(597, 113)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(144, 76)
+    Me.Label1.TabIndex = 4
+    Me.Label1.Text = "000"
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(787, 551)
+    Me.Controls.Add(Me.Label1)
+    Me.Controls.Add(Me.Button2)
+    Me.Controls.Add(Me.txtValue)
+    Me.Controls.Add(Me.Button1)
+    Me.Controls.Add(Me.PictureBox1)
+    Me.Name = "Form1"
+    Me.Text = "Form1"
+    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+  Friend WithEvents Button1 As System.Windows.Forms.Button
+  Friend WithEvents txtValue As System.Windows.Forms.TextBox
+  Friend WithEvents Button2 As System.Windows.Forms.Button
+  Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+  Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+    'Infos von http://upload.querysave.de/code.html
+    'Ravensburger Navigation http://www.ravensburger.de/content/wcm/mediadata/PDF/01_Kinder/tiptoicode_Navigationsleiste.pdf
+
+
+
+
+
+    PictureBox1.BackColor = Color.White
+    Dim g As Graphics = PictureBox1.CreateGraphics
+    Dim iMatrix(40, 40) As Integer
+    Dim iData(3, 3) As Integer
+    Dim iValue As Integer = Integer.Parse(txtValue.Text)
+    Dim tValue As Integer
+
+    Dim bruX As Brush
+
+    g.Clear(PictureBox1.BackColor)
+    Application.DoEvents()
+
+    'Grundmatrix
+    iMatrix(1, 1) = 1 : iMatrix(2, 1) = 1
+    iMatrix(1, 2) = 1 : iMatrix(2, 2) = 1
+
+    iMatrix(1, 13) = 1 : iMatrix(2, 13) = 1
+    iMatrix(1, 14) = 1 : iMatrix(2, 14) = 1
+
+    iMatrix(4, 25) = 1 : iMatrix(5, 25) = 1
+    iMatrix(4, 26) = 1 : iMatrix(5, 26) = 1
+
+    iMatrix(1, 37) = 1 : iMatrix(2, 37) = 1
+    iMatrix(1, 38) = 1 : iMatrix(2, 38) = 1
+
+
+    iMatrix(13, 1) = 1 : iMatrix(14, 1) = 1
+    iMatrix(13, 2) = 1 : iMatrix(14, 2) = 1
+
+
+    iMatrix(25, 1) = 1 : iMatrix(26, 1) = 1
+    iMatrix(25, 2) = 1 : iMatrix(26, 2) = 1
+
+    iMatrix(37, 1) = 1 : iMatrix(38, 1) = 1
+    iMatrix(37, 2) = 1 : iMatrix(38, 2) = 1
+
+    'Daten in 4er Setzen
+
+    tValue = iValue
+    iData(3, 3) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(2, 3) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(1, 3) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+
+    iData(3, 2) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(2, 2) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(1, 2) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(3, 1) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+    iData(2, 1) = (tValue Mod 4)
+    tValue = Fix(tValue / 4)
+
+
+
+    
+
+
+    Dim check As Integer
+
+
+
+    check = (((iValue >> 2) Xor (iValue >> 8) Xor (iValue >> 12) Xor (iValue >> 14)) And 1) << 1
+    check = check Or (((iValue) Xor (iValue >> 4) Xor (iValue >> 6) Xor (iValue >> 10)) And 1)
+    check = check Xor 2
+    iData(1, 1) = check
+
+    'check = iValue Mod 8
+    'Select Case check
+    '  Case 4, 6 : iData(1, 1) = 0 - IIf(iValue Mod 32 >= 16, 1, 0)
+    '  Case 5, 7 : iData(1, 1) = 1 - IIf(iValue Mod 32 >= 16, 1, 0)
+    '  Case 0, 2 : iData(1, 1) = 2 + IIf(iValue Mod 32 >= 16, 1, 0)
+    '  Case 1, 3 : iData(1, 1) = 3 + IIf(iValue Mod 32 >= 16, 1, 0)
+    'End Select
+    'If iData(1, 1) > 3 Then iData(1, 1) = 2
+    'If iData(1, 1) < 0 Then iData(1, 1) = 1
+
+    Label1.Text = iData(1, 1).ToString
+
+
+    'Daten in den Matrix eintragen
+    Dim iMx As Integer
+    Dim iMy As Integer
+    For x = 1 To 3
+      For y = 1 To 3
+        Select Case iData(x, y)
+          Case 0 : iMx = 3 : iMy = 3
+          Case 1 : iMx = -1 : iMy = 3
+          Case 2 : iMx = -1 : iMy = -1
+          Case 3 : iMx = 3 : iMy = -1
+        End Select
+        iMatrix(12 * x + iMx, 12 * y + iMy) = 2 : iMatrix(12 * x + iMx + 1, 12 * y + iMy) = 2
+        iMatrix(12 * x + iMx, 12 * y + iMy + 1) = 2 : iMatrix(12 * x + iMx + 1, 12 * y + iMy + 1) = 2
+
+      Next
+    Next
+
+
+
+    Dim iFactor As Integer = 10
+    For x = 1 To 40
+      For y = 1 To 40
+        bruX = Brushes.Gray
+        Select Case iMatrix(x, y)
+          Case 1 : bruX = Brushes.Red
+          Case 2 : bruX = Brushes.Black
+        End Select
+        g.FillRectangle(bruX, iFactor * (x - 1), iFactor * (y - 1), iFactor, iFactor)
+        g.DrawRectangle(Pens.AliceBlue, iFactor * (x - 1), iFactor * (y - 1), iFactor, iFactor)
+      Next
+    Next
+
+    g.Dispose()
+
+
+
+  End Sub
+  
+  Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+    txtValue.Text = (Integer.Parse(txtValue.Text) + 1).ToString
+    Button1_Click(Nothing, Nothing)
+
+
+  End Sub
+End Class
